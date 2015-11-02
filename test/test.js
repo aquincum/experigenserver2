805208fc508994da29c3a1f9ea62facdd48ff4ef
aquincum <daniel@szeredi.hu>
2015-11-02 01:53:24 -0500

    First funny unit test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,15 @@
 var assert = require("assert");
+var routing = require("../routing");
 
 // let's do this
-describe();
+/* first */
+describe("Routing", function(){
+    it("Should route to functions to responses", function(){
+	var mockServer = {
+	    get: function(path, func){
+		assert.equal(typeof func, "function");
+	    }
+	};
+	routing(mockServer);
+    });
+});
