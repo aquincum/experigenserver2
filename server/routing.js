5b369cec9d8c11cec0deaff8639a23225a4bc987
aquincum <daniel@szeredi.hu>
2015-11-08 03:28:13 -0500

    dbwrite routing

--- server/routing.js
+++ server/routing.js
@@ -35,9 +35,42 @@ var getUserID = function(req, res){
     }
 };
 
+
+/**
+ * Writes data coming from the experiment into the database. Everything goes
+ * to a collection with the hashed name. Destination (eg. demographics.csv,
+ * etc.) is just a field for now, it'll be handled on download -- this is how
+ * Experigen server 1 did it too.
+ */
+var dbWrite = function(req, res){
+    function fail(){
+	res.end("(\"false\")"); // this is how the cgi died
+    }
+    if (!req.query) return fail();
+    // necessary fields
+    var ufn = req.query.userFileName,
+	userCode = req.query.userCode,
+	experimentName = req.query.experimentName,
+	sourceurl = req.query.sourceurl;
+
+    if (!ufn || !userCode || !experimentName || !sourceurl){
+	return fail();
+    }
+
+    // let's pass on everything now to the db, I'll clean up there.
+    if(db.write(res.query)){
+	res.end("(\"true\")");
+    }
+    else {
+	fail();
+    }
+};
+
+
 var routes = {
     "/version" : postVersion,
-    "/getuserid" : getUserID
+    "/getuserid" : getUserID,
+    "/dbwrite": dbWrite
 };
 
 /**
