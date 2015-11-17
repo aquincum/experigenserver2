45289dbaf9f2345de79c9ee8ec867f2ddabcb6ef
aquincum <daniel@szeredi.hu>
2015-11-17 00:26:21 -0500

    Check existence works flawlessly

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,8 +1,25 @@
 (function($){
     $(function(){
+        function respond(s){
+            $("#status").html(s);
+        }
         $("input[name=checkExistence]").click(function(){
-            $("#status").html("Hello.");
+            var req = "/users?";
+            respond("");
+            req += "sourceurl=" + $("input[name=sourceURL]").val();
+            req += "&experimentName=" + $("input[name=experimentName]").val();
+            $.get(req)
+                .done(function(data){
+                    if(data == "No such experiment!"){
+                        respond(data);
+                    }
+                    else {
+                        respond(data.split("\n").length - 2);
+                    }
+                })
+                .fail(function(){
+                    respond("API response error");
+                });
         });
     });
-        console.log("done");
 })(jQuery);
