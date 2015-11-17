bdfd9c4df0c3fd574437e7f69ad9e73f7ed6c3cf
aquincum <daniel@szeredi.hu>
2015-11-17 00:06:52 -0500

    Setting up frontend

---
+++ public/js/admin.js
@@ -0,0 +1,8 @@
+(function($){
+    $(function(){
+        $("input[name=checkExistence]").click(function(){
+            $("#status").html("Hello.");
+        });
+    });
+        console.log("done");
+})(jQuery);
