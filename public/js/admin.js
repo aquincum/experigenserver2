605320abc1460e116e9b652435512b978606f3f6
aquincum <daniel@szeredi.hu>
2015-12-02 17:12:44 -0500

    Client registration implemented

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,54 +1,56 @@
+function getDestination(){
+    var desttxt = $("input[name=destination]").val();
+    var destsel = $("select#destinationdropdown").val();
+    if (desttxt !== ""){
+        return desttxt;
+    }
+    if (destsel !== null){
+        return destsel;
+    }
+    return "";
+}
+
+function apiCall(request, callback){
+    var req = "/" + request + "?",
+        dest = getDestination();
+    respond("");
+    req += "sourceurl=" + $("input[name=sourceURL]").val();
+    req += "&experimentName=" + $("input[name=experimentName]").val();
+    if(dest !== ""){
+        req += "&file=" + dest;
+    }
+
+    $.get(req)
+        .done(function(data){
+            if(data == "No such experiment!"){
+                respond(data, "danger");
+            }
+            else {
+                callback(data);
+            }
+        })
+        .fail(function(){
+            respond("API response error", "danger");
+        });
+}
+
+function respond(s, alertstatus){
+    $("#status").html(s);
+    $("#status").removeClass("alert-info alert-danger alert-warning alert-success");
+    if(alertstatus){
+        $("#status").addClass("alert-" + alertstatus);
+    }
+}
+$("input[name=checkExistence]").click(function(){
+    apiCall("users", function(data){
+        respond("<strong>Experiment exists.</strong> There are " + (data.split("\n").length - 2) + " users in the database.", "success");
+    });
+});
+
+
 (function($){    
     $(function(){
 
-        function getDestination(){
-            var desttxt = $("input[name=destination]").val();
-            var destsel = $("select#destinationdropdown").val();
-            if (desttxt !== ""){
-                return desttxt;
-            }
-            if (destsel !== null){
-                return destsel;
-            }
-            return "";
-        }
-        
-        function apiCall(request, callback){
-            var req = "/" + request + "?",
-                dest = getDestination();
-            respond("");
-            req += "sourceurl=" + $("input[name=sourceURL]").val();
-            req += "&experimentName=" + $("input[name=experimentName]").val();
-            if(dest !== ""){
-                req += "&file=" + dest;
-            }
-
-            $.get(req)
-                .done(function(data){
-                    if(data == "No such experiment!"){
-                        respond(data, "danger");
-                    }
-                    else {
-                        callback(data);
-                    }
-                })
-                .fail(function(){
-                    respond("API response error", "danger");
-                });
-        }
-
-        function respond(s, alertstatus){
-            $("#status").html(s);
-            $("#status").removeClass("alert-info alert-danger alert-warning alert-success");
-            if(alertstatus){
-                $("#status").addClass("alert-" + alertstatus);
-            }
-        }
-        $("input[name=checkExistence]").click(function(){
-            apiCall("users", function(data){
-                respond("<strong>Experiment exists.</strong> There are " + (data.split("\n").length - 2) + " users in the database.", "success");
-            });
-        });
         $("input[name=getData]").click(function(){
             apiCall("makecsv", function(data){
                 respond("<strong>Success!</strong> Data download should start right away.", "success");
@@ -83,4 +85,33 @@
     });
 })(jQuery);
 
+
 var app = angular.module("adminApp", []);
+app.controller("experimenterCtrl", function($scope){
+    $scope.experimenter = "";
+    $scope.password = "";
+    $scope.ha1 = "";
+    $scope.updateHA1 = function(){
+        $scope.ha1 = CryptoJS.MD5($scope.experimenter + ":Experimenters" + $scope.password).toString();
+    };
+    $scope.$watch("experimenter", $scope.updateHA1);
+    $scope.$watch("password", $scope.updateHA1);
+    $scope.register = function(){
+        var req = "/experimenter?experimenter=" + $scope.experimenter +
+            "&ha1="+$scope.ha1;
+        $.post(req).success(function(data){
+            respond($scope.experimenter + " registered!", "success");
+        }).fail(function(data){
+            switch(data.status){
+            case 400:
+                respond(data, "danger");
+                break;
+            case 409:
+                respond("Experimenter already exists!", "danger");
+                break;
+            default:
+                respond("Unknown error: " + data.responseText, "danger");
+            }
+        });
+    };
+});
